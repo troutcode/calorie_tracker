884692067aa0984dc7a11023cab444864fc676d8
neno <imkannachan@gmail.com>
2026-03-19 01:35:46 +0700

    Remove unnecessary library

--- app/src/main/java/com/example/caltracker/MainActivity.java
+++ app/src/main/java/com/example/caltracker/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
